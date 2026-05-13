791a7633ed435957c35708e3f2512427cb6cc9ed
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-05-13 16:24:59 +0200

    add basic login screen

--- app/src/main/java/com/example/android_app/MainActivity.java
+++ app/src/main/java/com/example/android_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
