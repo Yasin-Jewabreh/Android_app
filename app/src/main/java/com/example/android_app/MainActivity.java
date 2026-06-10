cbeca7a2d246f8453d287f677026ebf327024c17
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-10 21:40:50 +0200

    adding homescreen; check that email and password is not empty before switching activity

--- app/src/main/java/com/example/android_app/MainActivity.java
+++ app/src/main/java/com/example/android_app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android_app;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -20,11 +23,42 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        final Button loginButton = findViewById(R.id.loginButton);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+
+                if (checkCredentials()) {
+                    Toast.makeText(view.getContext(), "Login erfolgreich!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
         });
+
+    }
+
+    public boolean checkCredentials() {
+        boolean status = false;
+        EditText emailField = findViewById(R.id.emailText);
+        EditText passwordField = findViewById(R.id.passwortText);
+        String emailText = emailField.getText().toString().trim();
+        String passwordText = passwordField.getText().toString().trim();
+
+        if (emailText.isEmpty()) {
+            emailField.setError("Bitte füllen Sie dieses Feld aus!");
+        }
+        if (passwordText.isEmpty()) {
+            passwordField.setError("Bitte füllen Sie dieses Feld aus!");
+        }
+        if (!emailText.isEmpty() && !passwordText.isEmpty()) {
+            status = true;
+        }
+        return status;
     }
 }
