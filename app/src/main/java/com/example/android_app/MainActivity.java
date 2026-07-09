403ce85ba0cec0993bd602a90611e9476999427c
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-07-09 23:40:01 +0200

    add register screeen plus forwarding

--- app/src/main/java/com/example/android_app/MainActivity.java
+++ app/src/main/java/com/example/android_app/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.android_app;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.*;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                     startActivity(intent);
                 }
+            }
+        });
+
+        final TextView registerText = findViewById(R.id.loginText);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
 
