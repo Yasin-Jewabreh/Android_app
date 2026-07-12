6e5aebba73035cee4e6b01894262552a874c7865
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-07-13 01:42:33 +0200

    user klasse erstellen

---
+++ app/src/main/java/com/example/android_app/User.java
@@ -0,0 +1,33 @@
+package com.example.android_app;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String passwort;
+
+    public User(String id, String name, String email, String passwort) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.passwort = passwort;
+    }
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+}
