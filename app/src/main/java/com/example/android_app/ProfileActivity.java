5ba700edadf004b31c657bd5346cdc92aa8095a1
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-08-14 21:13:20 +0200

    add logic for logout

--- app/src/main/java/com/example/android_app/ProfileActivity.java
+++ app/src/main/java/com/example/android_app/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.example.android_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class ProfileActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +26,27 @@ public class ProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnLogout = findViewById(R.id.btnLogout);
 
+        mAuth = FirebaseAuth.getInstance();
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new com.google.android.material.dialog.MaterialAlertDialogBuilder(
+                        ProfileActivity.this)
+                        .setTitle("Abmelden")
+                        .setMessage("Möchtest du dich wirklich abmelden?")
+                        .setNegativeButton("Abbrechen", null)
+                        .setPositiveButton("Abmelden", (dialog, which) -> {
+
+                            mAuth.signOut();
+                            Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }).show();
+                };
+        });
 
         com.google.android.material.bottomnavigation.BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
 
